b7d24a9b50a75f1bc4feba958568387251787e54
Julian Lods <julian.lods@gmail.com>
2025-05-20 15:25:10 -0300

    Fix: protección de admin de Contenido ante errores de Cloudinary

--- toth/admin.py
+++ toth/admin.py
@@ -186,6 +186,38 @@ class ContenidoAdmin(admin.ModelAdmin):
         super().save_model(request, obj, form, change)
 
 
+@admin.register(Contenido)
+class ContenidoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'categoria', 'mostrar_video_url', 'mostrar_video_archivo', 'mostrar_pdf')
+    list_filter = ('categoria',)
+    search_fields = ('titulo',)
+
+    def mostrar_video_url(self, obj):
+        return obj.video_url if obj.video_url else "(sin url)"
+    mostrar_video_url.short_description = "Video URL"
+
+    def mostrar_video_archivo(self, obj):
+        try:
+            return obj.video_archivo.url if obj.video_archivo else "(sin archivo)"
+        except:
+            return "(error archivo)"
+    mostrar_video_archivo.short_description = "Video Archivo"
+
+    def mostrar_pdf(self, obj):
+        try:
+            return obj.archivo_pdf.url if obj.archivo_pdf else "(sin pdf)"
+        except:
+            return "(error pdf)"
+    mostrar_pdf.short_description = "PDF"
+
+    def save_model(self, request, obj, form, change):
+        # Convertir shorts de YouTube a enlaces normales
+        if obj.video_url and "youtube.com/shorts/" in obj.video_url:
+            video_id = obj.video_url.split("youtube.com/shorts/")[-1].split("?")[0].split("/")[0]
+            obj.video_url = f"https://www.youtube.com/watch?v={video_id}"
+        super().save_model(request, obj, form, change)
+
+
 @admin.register(Novedad)
 class NovedadAdmin(admin.ModelAdmin):
     list_display = ('id', 'descripcion', 'fecha', 'clasificacion', 'get_estado')
